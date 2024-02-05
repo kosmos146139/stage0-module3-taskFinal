c3d676aa4e30e3800370e9d8e862e31be8416ea6
Artur Kosmovskij <kosmos146139@gmail.com>
2024-02-05 11:55:11 +0300

    112

--- src/main/java/lang/print/gaps/finalModuleTask/DivisionByZero.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DivisionByZero.java
@@ -2,7 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class DivisionByZero {
     public static void main(String[] args) {
-        _ number = 10;
+        double number = 10;
+        number = Double.POSITIVE_INFINITY;
         System.out.println(number/0);
     }
 }
